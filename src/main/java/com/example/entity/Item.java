f2b69f5c7f413b635f2b345c18a23faee336e4b6
geass <takeru.kojima@intra.remtiro.com>
2023-06-02 14:05:50 +0900

    c2l2

--- src/main/java/com/example/entity/Item.java
+++ src/main/java/com/example/entity/Item.java
@@ -1,14 +1,10 @@
 package com.example.entity;
 
-import java.time.LocalDateTime;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -16,32 +12,11 @@ import javax.persistence.Table;
 @Table(name = "ITEMS")
 public class Item {
 
-	@ManyToOne
-	    @JoinColumn(name = "category_id", insertable = false, updatable = false)
-	    private Category category;
-
-	    public Category getCategory() {
-	        return this.category;
-	}
-	@Id
+    @Id
     @SequenceGenerator(name = "ITEM_ID_GENERATOR", sequenceName = "ITEM_ID_SEQ", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ITEM_ID_GENERATOR")
     @Column(name = "ID")
     private Integer id;
-
-	@Column(name = "CATEGORY_ID")
-	private Integer categoryId;
-
-	public Integer getCategoryId() {
-	        return this.categoryId;
-	}
-
-	public void setCategoryId(Integer categoryId) {
-	        this.categoryId = categoryId;
-	}
-
-	@Column(name = "DELETED_AT")
-    private LocalDateTime deletedAt;
 
     @Column(name = "NAME")
     private String name;
@@ -72,21 +47,5 @@ public class Item {
 
     public void setPrice(Integer price) {
         this.price = price;
-    }
-    public LocalDateTime getDeletedAt() {
-        return this.deletedAt;
-    }
-
-    public void setDeletedAt(LocalDateTime deletedAt) {
-        this.deletedAt = deletedAt;
-    }
-    private Integer stock;
-
-    public Integer getStock() {
-        return this.stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
     }
 }
